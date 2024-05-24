fe55846fd4dddd2f38a3979d3c6dd72dc80700fe
Saksham Arora <140829150+SakshamArora07@users.noreply.github.com>
2024-05-24 21:08:45 +0530

    Add files via upload

---
+++ Python/Python Basics/Module.py
@@ -0,0 +1,41 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "id": "e7c0727b-fa8c-4e65-9851-4782a118bf0b",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "def sum(a,b):\n",
+    "    c = a + b\n",
+    "    return c\n",
+    "\n",
+    "def mult(a,b):\n",
+    "    c = a * b\n",
+    "    return c"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.11.4"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
